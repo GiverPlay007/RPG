56781a239bbdca15b89c66e105c17ab3ef717bbb
GiverPlay007 <play.giver.007@gmail.com>
2021-11-21 12:47:26 -0300

    SDK configurada

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
+    id("com.github.johnrengelman.shadow") version "7.0.0"
     kotlin("jvm") version "1.5.10"
     java
 }
@@ -8,10 +11,13 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+    api("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
 }
 
 tasks.processResources {
@@ -20,5 +26,15 @@ tasks.processResources {
 
     filesMatching("plugin.yml") {
         expand(props)
+    }
+}
+
+tasks {
+    val shadowJar = named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("RPG")
+    }
+
+    "build" {
+        dependsOn(shadowJar)
     }
 }
---
+++ src/main/java/me/giverplay/rpg/RPG.kt
@@ -0,0 +1,9 @@
+package me.giverplay.rpg
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RPG: JavaPlugin() {
+    override fun onEnable() {
+        logger.info("Sup World")
+    }
+}
