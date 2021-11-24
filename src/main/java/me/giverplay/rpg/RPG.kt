df0d579a69302b3f5345b75286763aff10fc44df
GiverPlay007 <play.giver.007@gmail.com>
2021-11-23 22:19:35 -0300

    Código formatado

--- src/main/java/me/giverplay/rpg/RPG.kt
+++ src/main/java/me/giverplay/rpg/RPG.kt
@@ -2,8 +2,8 @@ package me.giverplay.rpg
 
 import org.bukkit.plugin.java.JavaPlugin
 
-class RPG: JavaPlugin() {
-    override fun onEnable() {
-        logger.info("Sup World")
-    }
+class RPG : JavaPlugin() {
+  override fun onEnable() {
+    logger.info("Sup World")
+  }
 }
